7d07ac9294671859187a0827a5ec8bc35871260a
Thomas <thomas.lilley01@gmail.com>
2022-12-02 01:07:58 +0000

    2015 day 3 task 2

--- 2015/day-3/advent15-03-solution.dart
+++ 2015/day-3/advent15-03-solution.dart
@@ -38,8 +38,35 @@ void solutionOne() {
     _positions.putIfAbsent(Point(xPos, yPos), () => 0);
     _positions[Point(xPos, yPos)] = _positions[Point(xPos, yPos)]! + 1;
   };
-
   print("Solution one: ${_positions.values.length}");
 }
 
-void solutionTwo() {}
+void solutionTwo() {
+  //Santa and Robo Santa begins by delivering a present to the house at the starting location (0,0)
+  Map<Point, int> _positions = {Point(0,0): 2}; 
+  int xPos = 0;
+  int yPos = 0;
+  int xRoboPos = 0;
+  int yRoboPos = 0;
+
+  for(int i = 0; i < input.length; i++){
+    switch (input[i]) {
+      case "^":
+        i.isEven ? yPos++ : yRoboPos++;
+        break;
+      case ">":
+         i.isEven ? xPos++ : xRoboPos++;
+        break;
+      case "v":
+         i.isEven ? yPos-- : yRoboPos--;
+        break;
+      case "<":
+         i.isEven ? xPos-- : xRoboPos--;
+        break;
+    }
+
+    _positions.putIfAbsent( i.isEven ? Point(xPos, yPos) : Point(xRoboPos, yRoboPos), () => 0);
+    _positions[i.isEven ? Point(xPos, yPos) : Point(xRoboPos, yRoboPos)] = _positions[i.isEven ? Point(xPos, yPos) : Point(xRoboPos, yRoboPos)]! + 1;
+  };
+  print("Solution two: ${_positions.values.length}");
+}
