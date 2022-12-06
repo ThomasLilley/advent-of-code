05083bee1cfb69d2a43ddf7a355128542cc359a1
Thomas <thomas.lilley01@gmail.com>
2022-12-06 14:41:34 +0000

    Aoc day 5 - task 2

--- 2022/day-5/advent22-05-solution.dart
+++ 2022/day-5/advent22-05-solution.dart
@@ -30,7 +30,7 @@ void solutionOne() {
     }
   }
 
-///iterate thorugh each movement and manipulate the map
+  ///iterate thorugh each movement and manipulate the map
   for (int i = 0; i < _movements.length; i++) {
     List<int> _instructions = _movements[i].replaceAll(RegExp(r'(move )|(from )|(to )'), '').split(' ').map((e) => e.trim()).toList().map((e) => int.parse(e)).toList();
 
@@ -40,13 +40,44 @@ void solutionOne() {
     }
   }
 
-///build output from top of each stack
-String _output = "";
-_boxes.forEach((key, value) => _output += value.last);
+  ///build output from top of each stack
+  String _output = "";
+  _boxes.forEach((key, value) => _output += value.last);
 
   print("Solution one: $_output");
 }
 
 void solutionTwo() {
-  print("Solution two: ");
+  List<String> _stacks = _input.first.split("\n");
+  List<String> _movements = _input.last.split("\n");
+  Map<int, List<String>> _boxes = {};
+
+//parse boxes into useable map
+  for (int i = 0; i < _stacks.length - 1; i++) {
+    int _count = 1;
+    for (int j = 0; j < _stacks[i].length; j += 4) {
+      String _box = _stacks[i].substring(j, (j + 4) > _stacks[i].length ? _stacks[i].length : j + 4).substring(1, 2);
+      _boxes.putIfAbsent(_count, () => []);
+      if (!_box.contains(' ')) {
+        _boxes[_count]!.insert(0, _box);
+      }
+      _count++;
+    }
+  }
+
+  ///iterate thorugh each movement and manipulate the map
+  for (int i = 0; i < _movements.length; i++) {
+    List<int> _instructions = _movements[i].replaceAll(RegExp(r'(move )|(from )|(to )'), '').split(' ').map((e) => e.trim()).toList().map((e) => int.parse(e)).toList();
+    List<String> _boxesToMove = [];
+    for (int boxCount = 0; boxCount < _instructions.first; boxCount++) {
+      _boxesToMove.insert(0, _boxes[_instructions[1]]!.removeLast());
+    }
+    _boxes[_instructions[2]]!.addAll(_boxesToMove);
+  }
+
+  ///build output from top of each stack
+  String _output = "";
+  _boxes.forEach((key, value) => _output += value.last);
+
+  print("Solution two: $_output");
 }
