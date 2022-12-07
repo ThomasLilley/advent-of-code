8a8b1fa622476215b5678c3194983a1597cd73ae
Thomas <thomas.lilley01@gmail.com>
2022-12-07 23:27:53 +0000

    day 7 task 1 complete

--- 2022/day-7/advent22-07-solution.dart
+++ 2022/day-7/advent22-07-solution.dart
@@ -1,17 +1,17 @@
 import 'dart:io';
 
 List<String> _input = [];
-TreeNode<String>? _filesystem;
-TreeNode<String>? _currentNode;
+Directory<String>? _filesystem;
+Directory<String>? _currentNode;
 
-class TreeNode<T> {
-  TreeNode<T>? parent;
+class Directory<T> {
+  Directory<T>? parent;
   T value;
-  List<TreeNode<T>>? children;
+  List<Directory<T>>? children;
   List<ElfFile>? files;
-  TreeNode(this.value, {this.parent, this.children, this.files});
+  Directory(this.value, {this.parent, this.children, this.files});
 
-  void add(TreeNode<T> child) {
+  void add(Directory<T> child) {
     children ??= [];
     if (!children!.contains(child)) {
       children!.add(child);
@@ -23,11 +23,18 @@ class TreeNode<T> {
     files!.add(file);
   }
 
+  int getDirectorySize() {
+    int size = 0;
+    if(files != null)files!.forEach((element) => size += element.size);
+    if(children!=null)children!.forEach((element) => size+= element.getDirectorySize());
+    return size;
+  }
+
   void printTree() {
     int level = 0;
     String _tabs = "";
 
-    TreeNode element = this;
+    Directory element = this;
     while (element.parent != null) {
       element = element.parent!;
       level++;
@@ -44,13 +51,21 @@ class TreeNode<T> {
     files?.forEach((e) => print("  ${_tabs}$e "));
   }
 
+  int sumDirectories(int ofSize){
+    int size = 0;
+    int _temp = getDirectorySize();
+    if(_temp <= ofSize) size += _temp;
+     children?.forEach((element) => size += element.sumDirectories(ofSize));
+    return size;
+  }
+
   @override
-  bool operator ==(o) => o is TreeNode && value == o.value;
+  bool operator ==(o) => o is Directory && value == o.value;
   int get hashCode => value.hashCode;
 
   @override
   String toString() {
-    return "$value (dir)";
+    return "$value (dir, size=${getDirectorySize()})";
   }
 }
 
@@ -77,37 +92,34 @@ void main() {
 }
 
 void readFile() {
-  _input = File('2022/day-7/advent22-07-example.txt').readAsStringSync().split("\n");
+  _input = File('2022/day-7/advent22-07-input.txt').readAsStringSync().split("\n");
 }
 
 void solutionOne() {
   for (int i = 0; i < _input.length; i++) {
     if (_input[i].startsWith("\$")) {
       if (_input[i] == "\$ cd /") {
-        _currentNode = _filesystem = TreeNode("/");
+        _currentNode = _filesystem = Directory("/");
       } else if (_input[i].startsWith("\$ cd")) {
         if (_input[i] == "\$ cd ..") {
           _currentNode = _currentNode?.parent;
         } else {
           String _dir = _input[i].replaceAll("\$ cd ", "");
-          TreeNode<String> _newNode = TreeNode<String>(_dir, parent: _currentNode);
+          Directory<String> _newNode = Directory<String>(_dir, parent: _currentNode);
           _currentNode?.add(_newNode);
           _currentNode = _currentNode?.children?.firstWhere((element) => element == _newNode);
         }
       }
     } else if (_input[i].startsWith("dir")) {
       String _dir = _input[i].replaceAll("dir ", "");
-      _currentNode?.add(TreeNode<String>(_dir, parent: _currentNode));
+      _currentNode?.add(Directory<String>(_dir, parent: _currentNode));
     } else {
       _currentNode?.addFile(ElfFile.fromString(_input[i]));
     }
   }
   _filesystem?.printTree();
+  print("Solution one: ${_filesystem?.sumDirectories(100000)}");
 }
 
-///add first level to map
-///access first level
-///add next level to map
-///repeat until bottom
 
 void solutionTwo() {}
