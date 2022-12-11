693b04eae7a4a145b81494b1ef91cf37dc4b3ab2
Thomas <thomas.lilley01@gmail.com>
2022-12-11 20:13:21 +0000

    refactoring to support part 2 - complete

--- 2022/day-9/advent22-09-solution.dart
+++ 2022/day-9/advent22-09-solution.dart
@@ -2,23 +2,20 @@ import 'dart:io';
 import 'dart:math';
 
 List<String> _input = [];
-List<int> _currentHeadPos = [0, 0];
-List<int> _currentTailPos = [0, 0];
-var touched = {Point(0,0)};
 
 void main() {
   readFile();
-  solutionOne();
-  solutionTwo();
+  print("Solution one: ${moveRope(2)}");
+  print("Solution two: ${moveRope(10)}");
 }
 
 void readFile() {
   _input = File('2022/day-9/advent22-09-input.txt').readAsStringSync().split("\n");
 }
 
-void solutionOne() {
- 
-
+int moveRope(int knots) {
+  var touched = {Point(0, 0)};
+  var rope = List.generate(knots, (i) => Point(0, 0));
   _input.forEach((element) {
     List<String> _instruction = element.split(" ");
     String _direction = _instruction.first;
@@ -27,44 +24,41 @@ void solutionOne() {
     for (int i = 0; i < _length; i++) {
       switch (_direction) {
         case "L":
-          _currentHeadPos[0]--;
+          rope.first = Point(rope.first.x - 1, rope.first.y);
           break;
         case "R":
-          _currentHeadPos[0]++;
+          rope.first = Point(rope.first.x + 1, rope.first.y);
           break;
         case "U":
-          _currentHeadPos[1]++;
+          rope.first = Point(rope.first.x, rope.first.y + 1);
           break;
         case "D":
-          _currentHeadPos[1]--;
+          rope.first = Point(rope.first.x, rope.first.y - 1);
           break;
       }
-    
-      int _xDistance = (_currentHeadPos[0] - _currentTailPos[0]);
-      int _yDistance = (_currentHeadPos[1] - _currentTailPos[1]);
 
-      if (max(_xDistance.abs(), _yDistance.abs()) > 1) {
-        if (_xDistance.abs() >= 1) {
-          if (_xDistance < 0) {
-            _currentTailPos = [_currentTailPos[0] - 1, _currentTailPos[1]];
-          } else {
-            _currentTailPos = [_currentTailPos[0] + 1, _currentTailPos[1]];
+      for (var j = 1; j <= knots - 1; j++) {
+        var x = rope[j - 1].x - rope[j].x;
+        var y = rope[j - 1].y - rope[j].y;
+        if (max(x.abs(), y.abs()) > 1) {
+          if (x.abs() >= 1) {
+            if (x < 0) {
+              rope[j] = Point(rope[j].x - 1, rope[j].y);
+            } else {
+              rope[j] = Point(rope[j].x + 1, rope[j].y);
+            }
           }
-        }
-        if (_yDistance.abs() >= 1) {
-          if (_yDistance < 0) {
-            _currentTailPos = [_currentTailPos[0], _currentTailPos[1] - 1];
-          } else {
-            _currentTailPos = [_currentTailPos[0], _currentTailPos[1] + 1];
+          if (y.abs() >= 1) {
+            if (y < 0) {
+              rope[j] = Point(rope[j].x, rope[j].y - 1);
+            } else {
+              rope[j] = Point(rope[j].x, rope[j].y + 1);
+            }
           }
         }
       }
-      touched.add(Point(_currentTailPos[0], _currentTailPos[1]));
-   
+      touched.add(rope.last);
     }
-  });  
-  print("Solution one: ${touched.length}");
+  });
+  return touched.length;
 }
-
-void solutionTwo() {}
-
