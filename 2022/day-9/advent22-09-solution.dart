7e9e00a0a6928dd0ccc80ec6a147cd3d4b636f4d
Thomas <thomas.lilley01@gmail.com>
2022-12-11 19:15:05 +0000

    remove objects and fix broken movement math

--- 2022/day-9/advent22-09-solution.dart
+++ 2022/day-9/advent22-09-solution.dart
@@ -1,9 +1,10 @@
-import 'dart:ffi';
 import 'dart:io';
+import 'dart:math';
 
 List<String> _input = [];
 List<int> _currentHeadPos = [0, 0];
 List<int> _currentTailPos = [0, 0];
+var touched = {Point(0,0)};
 
 void main() {
   readFile();
@@ -12,11 +13,11 @@ void main() {
 }
 
 void readFile() {
-  _input = File('2022/day-9/advent22-09-example.txt').readAsStringSync().split("\n");
+  _input = File('2022/day-9/advent22-09-input.txt').readAsStringSync().split("\n");
 }
 
 void solutionOne() {
-  Grid _grid = Grid.generate(10, 10);
+ 
 
   _input.forEach((element) {
     List<String> _instruction = element.split(" ");
@@ -24,92 +25,46 @@ void solutionOne() {
     int _length = int.parse(_instruction.last);
 
     for (int i = 0; i < _length; i++) {
-      switch (_instruction.first) {
+      switch (_direction) {
         case "L":
-          stdout.writeln("L");
           _currentHeadPos[0]--;
           break;
         case "R":
-          stdout.writeln("R");
           _currentHeadPos[0]++;
           break;
         case "U":
-          stdout.writeln("U");
           _currentHeadPos[1]++;
           break;
         case "D":
-          stdout.writeln("D");
           _currentHeadPos[1]--;
           break;
       }
-      _grid.grid[_grid.indexOf(_currentHeadPos[0], _currentHeadPos[1])].headVisited++;
-      
-      print(_grid);
+    
+      int _xDistance = (_currentHeadPos[0] - _currentTailPos[0]);
+      int _yDistance = (_currentHeadPos[1] - _currentTailPos[1]);
+
+      if (max(_xDistance.abs(), _yDistance.abs()) > 1) {
+        if (_xDistance.abs() >= 1) {
+          if (_xDistance < 0) {
+            _currentTailPos = [_currentTailPos[0] - 1, _currentTailPos[1]];
+          } else {
+            _currentTailPos = [_currentTailPos[0] + 1, _currentTailPos[1]];
+          }
+        }
+        if (_yDistance.abs() >= 1) {
+          if (_yDistance < 0) {
+            _currentTailPos = [_currentTailPos[0], _currentTailPos[1] - 1];
+          } else {
+            _currentTailPos = [_currentTailPos[0], _currentTailPos[1] + 1];
+          }
+        }
+      }
+      touched.add(Point(_currentTailPos[0], _currentTailPos[1]));
+   
     }
-  });
+  });  
+  print("Solution one: ${touched.length}");
 }
 
 void solutionTwo() {}
 
-class Grid {
-  int m;
-  int n;
-  List<GridElement> grid;
-  Grid(this.m, this.n, this.grid);
-  factory Grid.generate(int m, int n) {
-    List<GridElement> _grid = [];
-    for (int y = 0; y < m; y++) {
-      for (int x = 0; x < n; x++) {
-        _grid.add(GridElement(x, y));
-      }
-    }
-    return Grid(m, n, _grid);
-  }
-
-  GridElement getElement(int x, int y) {
-    return grid[(x) + (y * n)];
-  }
-
-  int indexOf(x, y){
-    return (x) + (y * n);
-  }
-
-  void setElement(int x, int y, GridElement element) {
-    grid[(x) + (y * n)] = element;
-  }
-
-  @override
-  String toString({bool withDivider = false}) {
-    StringBuffer buffer = StringBuffer();
-    List<List<GridElement>> _rows = [];
-    for (int i = 0; i < grid.length; i += n) {
-      _rows.insert(0, this.grid.sublist(i, i + n));
-      //buffer.write("${this.grid[i]} ${withDivider ? "," : ""}${(i + 1) % n == 0 ? "\n" : ""}");
-    }
-    _rows.forEach((row) {
-      row.forEach((element) {
-        buffer.write("$element ");
-      });
-      buffer.write("\n");
-    });
-    return buffer.toString();
-  }
-}
-
-class GridElement {
-  int x;
-  int y;
-  int headVisited;
-  int tailVisited;
-  GridElement(this.x, this.y, {this.headVisited = 0, this.tailVisited = 0});
-
-  @override
-  String toString() {
-    if (_currentHeadPos[0] == x && _currentHeadPos[1] == y &&  _currentTailPos[0] == x && _currentTailPos[1] == y) return "B";
-    if (_currentHeadPos[0] == x && _currentHeadPos[1] == y) return "H";
-    if (_currentTailPos[0] == x && _currentTailPos[1] == y) return "T";
-    if (tailVisited > 0) return "#";
-    if(headVisited > 0) return "%";
-    return ".";
-  }
-}
